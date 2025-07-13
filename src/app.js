b80edc3ce90632af945419a27c1939147e43c13c
Anurag <anurag1224kumar@outlook.com>
2025-07-13 23:58:06 +0530

    created the basic routes

--- src/app.js
+++ src/app.js
@@ -5,17 +5,20 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res) => {
-    res.send("Namaste from the dashboard");
-});
-
-
 app.use("/test",(req,res) => {
     res.send("Hello from the server");
 });
 
+app.use("/hello/2", (req, res) => {
+    res.send("Abrakadabra");
+});
+
 app.use("/hello", (req, res) => {
     res.send("Hello Hello Hello");
+});
+
+app.use("/",(req,res) => {
+    res.send("Namaste from the dashboard");
 });
 
 app.listen(7777, () => {
