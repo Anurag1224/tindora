bdb0f2fd9b6e7ebdae3708360d386283f50ae970
Anurag <anurag1224kumar@outlook.com>
2025-07-15 20:14:48 +0530

    playing with Routes

--- src/app.js
+++ src/app.js
@@ -10,28 +10,59 @@ app.use("/test",(req,res) => {
 });
 
 
-
-app.get("/user", (req, res) => {
+app.get(/.*fly$/, (req, res) => {
     res.send({firstName:"Anurag", lastName:"Kumar"});
 });
 
-app.post("/user", (req, res) => {
-    //console.log("Saved data to the database");
-    res.send("Data successfully saved to the database");
+//Dynamic route and req.params to get the parameters 
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+    
+    const obj = JSON.parse(JSON.stringify(req.params)); // if we do this then the response that is returned from the req.parmas will be a JS object
+    console.log(obj);
+    res.send({firstName:"Anurag", lastName:"Kumar"});
 });
 
-app.delete("/user", (req, res) => {
-    //console.log("data deleted from the database");
-    res.send("Data successfully deleted from the database");
-});
+// req.query to get the queries
 
-app.use("/user",(req,res) => {
-    res.send("HAHAHAHAHA");
-});
+// app.get("/user/:userId", (req, res) => {
+//     console.log(req.query); // here it will give the out put but it says [Object: null prototype] 
+//     res.send({firstName:"Anurag", lastName:"Kumar"});
+// });
+
+// app.get("/user", (req, res) => {
+//     res.send({firstName:"Anurag", lastName:"Kumar"});
+// });
+
+// app.post("/user", (req, res) => {
+//     //console.log("Saved data to the database");
+//     res.send("Data successfully saved to the database");
+// });
+
+// app.put("/user", (req, res) => {
+//     // console.log("Data updated sucessfully");
+//     res.send("Data updated sucessfully");
+// });
+
+// app.patch("/user", (req, res) => {
+//     //console.log("Particular data updated successfully");
+//     res.send("Particular data updated successfully");
+// });
+
+// app.delete("/user", (req, res) => {
+//     //console.log("data deleted from the database");
+//     res.send("Data successfully deleted from the database");
+// });
+
+// app.use("/user",(req,res) => {
+//     res.send("HAHAHAHAHA");
+// });
 
 // app.use("/",(req,res) => {
 //     res.send("Namaste from the dashboard");
 // });
+
+//Playing more with routes i.e advanced concept
 
 app.listen(7777, () => {
     console.log("My server is sucessfully listening on port 7777...");
