c13501afb0b4eb4cc744d742a1988cd10617a33d
Anurag <anurag1224kumar@outlook.com>
2025-07-19 22:24:20 +0530

    authorization through middlewares

--- src/app.js
+++ src/app.js
@@ -64,28 +64,105 @@ const app = express();
 
 //Playing more with routes i.e advanced concept
 
-app.use("/user", (req, res, next) => {
-    console.log("Handeling route user");
-    // res.send("Resonse 1");
-    next();
+// app.use("/user", [(req, res, next) => {
+//     console.log("Handeling route user");
+//     // res.send("Resonse 1");
+//     next();
 
-},
-(req, res, next) => {
-    console.log("Handeling route user 2");
-    // res.send("Resonse 2");
-    next();
-},
-(req, res, next) => {
-    console.log("Handeling route user 3");
-    // res.send("Resonse 3");
-    next();
-},
-(req, res, next) => {
-    console.log("Handeling route user 4");
-    // res.send("Resonse 4");
-    next();
-},
-);
+// },
+// (req, res, next) => {
+//     console.log("Handeling route user 2");
+//     res.send("Resonse 2");
+//     next();
+// },
+// (req, res, next) => {
+//     console.log("Handeling route user 3");
+//     // res.send("Resonse 3");
+//     next();
+// },
+// (req, res, next) => {
+//     console.log("Handeling route user 4");
+//     // res.send("Resonse 4");
+//     // next();
+// }],
+// );
+
+// app.use("/user",(req, res, next) => {
+//         console.log("Handeling route user");
+//         next();
+//     }
+// );
+
+// app.use("/user",(req, res, next) => {
+//         console.log("Handeling route user 2");
+//         res.send("Response 2");
+//     }
+// );
+
+// example to understand the middleware
+
+
+
+app.use("/admin", (req, res, next) => {
+
+    console.log("Admin authorization is being checked !!");
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if(!isAdminAuthorized){
+        res.status(401).send("Unauthorized Request")
+    }
+    else{
+        next();
+    }
+});
+
+app.get("/admin/getAllData", (req, res) => {
+
+        res.send("All data sent ");
+     
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+
+        res.send("Deleted a user");
+    
+});
+
+app.get("/user", (req, res) => {
+
+        res.send("User data sent ");
+     
+});
+
+
+
+// app.get("/admin/getAllData", (req, res) => {
+//     // write a logic to fetch all the data 
+//     const token = "xyz";
+//     isAdminAuthorized = token === "xyz";
+
+//     if(isAdminAuthorized){
+//         res.send("All data sent ");
+//     }
+//     else{
+//         res.status(401).send("Unauthorized Request");
+//     }
+    
+// })
+
+// app.get("/admin/deleteUser", (req, res) => {
+
+//     const token = "xyz2";
+//     isAdminAuthorized = token === "xyz";
+
+//     if(isAdminAuthorized){
+//         res.send("Deleted a user");
+//     }
+//     else{
+//         res.status(401).send("Unauthorized Request");
+//     }
+    
+// })
 
 app.listen(7777, () => {
     console.log("My server is sucessfully listening on port 7777...");
