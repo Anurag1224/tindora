f65df92bc047fa479f5d560877d9ee5e877ffd23
Anurag <anurag1224kumar@outlook.com>
2025-07-27 11:00:18 +0530

    created CRUD api for user

--- src/app.js
+++ src/app.js
@@ -25,6 +25,115 @@ app.post("/signUp", async (req, res) => {
 
 });
 
+//Feed API - GET /feed - to get all the users from the database
+app.get("/feed", async (req, res) => {
+
+    try{
+        const users = await User.find({}); //passing an empty filter
+        res.send(users);
+    }
+    catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+
+});
+
+//creating a GET api to get the user from a database based on their emailId
+app.get("/user", async (req , res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.findOne({emailId : userEmail});
+        if(users.length === 0){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+        
+    }
+    catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+app.get("/getUser", async (req , res) => {
+    const userId = req.body._id;
+
+    try{
+        console.log(userId)
+        const users = await User.findById(userId);
+        if(users.length === 0){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+        
+    }
+    catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+
+//Update data of a user using userId
+
+// app.patch("/user", async (req, res) => {
+
+//     const userId = req.body.userId;
+
+//     const data = req.body;
+
+//     try{
+//         // const user = await User.findByIdAndUpdate({_id : userId}, data);
+//         // OR
+//          const user = await User.findByIdAndUpdate( userId , data, {returnDocument: "after"});
+//          console.log(user);
+//         res.send("User data updated successfully");
+//     }
+//     catch(err){
+//         res.status(400).send("Something went wrong");
+//     }
+
+// });
+
+//Update data of a user using user EmailId
+app.patch("/user", async (req, res) => {
+
+    const userEmailId = req.body.emailId;
+
+    const data = req.body;
+    // console.log(data);
+
+    try{
+        // const user = await User.findByIdAndUpdate({_id : userId}, data);
+        // OR
+         const user = await User.findOneAndUpdate({emailId: userEmailId }, data, {returnDocument: "after"});
+         console.log(user);
+        res.send("User data updated successfully");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+
+});
+
+//delete user API - delete a user data from a database
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+
+    try{
+        // const user = await User.findByIdAndDelete({_id : userId}); 
+         //OR
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted Successfully");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+}); 
+
 connectDB().then(() =>{
     
     console.log("Database connection established.....");
