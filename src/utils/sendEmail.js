9b05852a31386f9e1170df9acb7452b597720415
Anurag <anurag1224kumar@outlook.com>
2025-08-28 13:23:32 +0530

    added sendEmail.js and sesClient.js to github

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,60 @@
+const { SendEmailCommand } = require("@aws-sdk/client-ses");
+const { sesClient } = require("./sesClient.js");
+
+
+
+const createSendEmailCommand = (toAddress, fromAddress, subject, body) => {
+  return new SendEmailCommand({
+    Destination: {
+      CcAddresses: [
+        
+      ],
+      ToAddresses: [
+        toAddress,
+        
+      ],
+    },
+    Message: {
+    
+      Body: {
+       
+        Html: {
+          Charset: "UTF-8",
+          Data: body,
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "This is the text format email.",
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: subject
+      },
+    },
+    Source: fromAddress,
+    ReplyToAddresses: [
+      
+    ],
+  });
+};
+
+const run = async (subject, body) => {
+  const sendEmailCommand = createSendEmailCommand(
+    "anurag1224kumar@gmail.com",
+    "support@themiraclestreet.co.in",
+    subject, body
+  );
+
+  try {
+    return await sesClient.send(sendEmailCommand);
+  } catch (caught) {
+    if (caught instanceof Error && caught.name === "MessageRejected") {
+      const messageRejectedError = caught;
+      return messageRejectedError;
+    }
+    throw caught;
+  }
+};
+
+module.exports = {run};
